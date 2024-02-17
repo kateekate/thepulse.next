4d8a8c884d6cc1f9c4d10579800d5796acf1b561
Katya <katya.mikhaylova.98@list.ru>
2024-02-17 21:04:44 +0100

    add fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
+import { gantari } from "./fonts";
 
 export const theme = extendTheme({
   colors: {
     background: "black",
   },
   fonts: {
-    body: "Arial, sans-serif",
-    heading: "Roboto, sans-serif",
+    body: gantari.style.fontFamily,
+    heading: gantari.style.fontFamily,
   },
   styles: {
     global: {
