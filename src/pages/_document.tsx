9193117e393a61e334d1ddaacb7778d5255bc254
Katya <katya.mikhaylova.98@list.ru>
2024-02-19 20:04:23 +0100

    add favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
